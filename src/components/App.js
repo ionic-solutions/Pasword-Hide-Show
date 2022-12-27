5df1c39c95b0b27ae9764282a653b9f5c4606931
ionic-solutions <ionicsolutions92@gmail.com>
2022-12-26 19:29:14 -0800

    add all files

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+import Form from "./components/Form";
+function App() {
+  return (
+    <div>
+      <Form />
+    </div>
+  );
+}
+export default App;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Form from '../components/Form';
+import './Home.css';
+
+class Home extends React.Component {
+ 
+  render() {
+
+
+    return (
+    <div>
+           <Form/>
+    </div>
+    );
+  }
+};
+export default Home;
